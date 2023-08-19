f19a5b8f2644392f3a76b27cedef12809a109587
selimdemirer <selimmdemirerr@gmail.com>
2023-08-19 20:55:57 +0300

    validation added

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,16 +1,58 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model) {
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
+
         return "employee/employee-create";
     }
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
+
+        // Maybe we can do some custom validations here!
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/employee/list"; // with redirect we are using end points (it is redirecting the method below, it is not going to view(html)!)
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+
+        model.addAttribute("employeeList",employeeService.readAllEmployees());
+
+        return "employee/employee-list"; // without redirect we are using html file paths
+    }
+
 
 }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,7 +1,12 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -11,8 +16,16 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+//    @NotNull      Field shouldn't be null
+//    @NotEmpty     Field shouldn't be ""
+//    @NotBlank     Field shouldn't be "   "
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String firstName;
     private String lastName;
+    //Thymeleaf accepts yyyy-mm-dd, but LocalDate mm-dd-yyyy  -  yyyy-dd-mm
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate birthday;
     private String email;
     private String password;
